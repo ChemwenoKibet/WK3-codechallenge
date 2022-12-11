3ca7f60f05ffa96c1c6bf7b856d47045a7c7a028
Ian <chemwenokibet11@gmail.com>
2022-12-11 15:45:44 +0300

    made changes to the js file

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ function getDisplayFilm(){
     //fetch film info
     fetch('http://localhost:3000/films')
     .then((res) => res.json())
-    .then(renderFirstMovie)
+    .then(renderFirstMovie) //hoisted function
+    
 }
 getDisplayFilm()
+
+function renderFirstMovie(firstMovie){
+    firstMovie.find(((element) => element.id === "1"))
+    firstFilm(firstMovie.find(((element) => element.id === "1")))
+}
