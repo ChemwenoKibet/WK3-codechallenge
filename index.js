a6ede2ee2345451a9b656fa0fe3e5f379e0e4e42
Ian <chemwenokibet11@gmail.com>
2022-12-12 01:51:32 +0300

    changes to js file

--- index.js
+++ index.js
@@ -8,4 +8,18 @@ fetch("http://localhost:3000/films")
     //display the poster
     const posterDiv = document.getElementById("moviePoster");
     let imageElement = document.createElement("img");
+
+    imageElement.src = firstFilm.poster;
+    imageElement.alt = "Poster image";
+    imageElement.width = "300";
+    imageElement.height = "400";
+    posterDiv.appendChild(imageElement);
+
+    //display first film
+    const titleAndRuntime = document.getElementById("titleAndRuntime");
+    let paraTitle = document.createElement("p")
+    let paraRuntime = document.createElement("p")
+
+    paraTitle.innerText = firstFilm.titile;
+    paraRuntime
 })
